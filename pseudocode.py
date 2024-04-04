27ec1156ce364fdad0718a5f38921a898f95820c
litaar <lalitahill77@gmail.com>
2024-04-04 14:32:38 +1100

    plan

---
+++ pseudocode.py
@@ -0,0 +1,31 @@
+BEGIN []
+print"choose your options:"
+print"2 - Miracle of compound interest"
+#User chooses
+Get choice
+While choice != 0
+
+
+#Function 1
+#Asking for one amount (income; annual, weekly, monthly)
+#break up the amount the user gives
+BEGIN Money buckets
+Get amount
+Blow = 60% x Amount
+Daily = 60% x Blow
+
+
+
+inputs
+#Ask for interest rate (every yr)
+#Ask the user to input their start age when they begin investing
+#Ask for the average annual interest return (e.g. 10%)
+#Ask for the amount they will invest every year
+#Show the projection of savings (with the dividends that get reinvested every year) until age 60
+
+3 inputs (Table)
+#Age you start investing
+#amount to invest every year
+#Average annual interest
+
+Age investment Total
