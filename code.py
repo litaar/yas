446729c0b4c7ef7c168c8f37cf6d5d044d2b202c
litaar <lalitahill77@gmail.com>
2024-04-11 09:09:47 +1000

    commit: made progress on money buckets and compound int pseudo commit: worked on python for monkey buckets and compound int commit: tried to work out how to print annually income for each age

--- code.py
+++ code.py
@@ -1,1 +1,52 @@
-print("Hello, World!")
+#BEGIN
+def Moneybuckets():
+    #Money buckets
+    Income = float(input("Income: "))
+    age = 60
+    #Calculate amounts
+    Blow = 0.6 * Income
+    Daily = 0.6 * Blow
+    Mojo = 0.2 * Blow
+    Grow = 0.2 * Blow
+    data_teams = ["Blow", "Mojo", "Grow"]
+    format_row = "{:>11}" * (len(data_teams) + 1)
+    print(format_row.format("", *data_teams))
+    print(Blow)
+    print(Mojo)
+    print(Grow)
+
+
+def Compoundinterest():
+    income = float(input("Annually investment: "))
+    interest = float(input("Average annual interest: "))
+    age = -2
+    while age < 59:
+        age = age + 1
+        print(age + 1)
+        for age in range(1,61):
+            print(income)
+
+
+
+    investment = income * 0.10 * interest
+
+
+    total = total + investment
+
+
+
+print("Money buckets (1)""  or  Miracle of compound interest (2)")
+choice = input("CHOOSE YOUR OPTIONS:")
+
+if choice == "1":
+    Moneybuckets()
+elif choice == "2":
+    Compoundinterest()
+else:
+    print("୨୧ Please choose either 1 or 2 ୨୧")
+    choice = input("CHOOSE YOUR OPTIONS:")
+
+
+#10 percent of current investment balance
+
+#format_row =
--- pseudocode.py
+++ pseudocode.py
@@ -4,15 +4,54 @@ print"2 - Miracle of compound interest"
 #User chooses
 Get choice
 While choice != 0
+Ask Money buckets or Compound interest
+if money buckets, run
+if compound interest, run
 
 
 #Function 1
 #Asking for one amount (income; annual, weekly, monthly)
 #break up the amount the user gives
 BEGIN Money buckets
-Get amount
-Blow = 60% x Amount
+Get Income
+Blow = 60% x Income
 Daily = 60% x Blow
+Mojo = 20% x Blow
+Grow = 20% x Blow
+In Blow calculate
+Splurge = 10% x Blow
+Smile = 10% x Blow
+Fire Extinguisher = 20% x Blow
+
+BEGIN Miracle of Compound Interest
+#User chooses
+Get age
+Get investment
+Get interest
+Display "Age of starting investment"
+Display "Annually investment"
+Display "Average annual interest"
+
+#while loop
+while age < 60
+#input calculations
+    total = total + investment
+    interest = total * 0.10
+    final = total + interest
+
+
+    age = age + 1
+
+
+
+#structure table
+age investment total
+
+
+Avg annual interest = 10%
+Display "projection of savings" until age 60
+
+
 
 
 
@@ -29,3 +68,16 @@ inputs
 #Average annual interest
 
 Age investment Total
+
+notes
+#Add for loops in pseudocode
+
+data dictionary
+#use variables from pseudocode
+-variables: choice (n/a), load inputs: daily expenses, age of investment
+validation
+amount they earn: float
+
+
+#data dictionary
+#make floats worth 4 bytes
